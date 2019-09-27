211be44e1c2875ead2afe7a78dd150e2c9c61b5b
aidan-mccarthy <amccarthy.business@gmail.com>
2019-09-26 22:30:39 -0400

    Update 0.3.0
    [#] Migration to Heroku
    [#] Addition of bootstrap
    [#] Cosmetic updates with move
    [-] Database no longer in a working state
    [-] TODO: Integration of postgresql

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
+// Global Setup
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+// Database Pool Setup
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
 });
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send(err);
-    }
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// Express Setup
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Routing
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+app.get('/academics', function(req, res) {
+  res.render('pages/academics');
+});
+
+app.get('/military', function(req, res) {
+  res.render('pages/military');
+});
+
+app.get('/physical', function(req, res) {
+  res.render('pages/physical');
+});
+
+app.get('/db', async(req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null };
+    res.render('pages/db', results);
+    client.release();
+  }
+
+  catch (err) {
+    console.error(err);
+    res.send(err);
+  }
+});
+
+// Server Listen
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));

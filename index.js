0541ce3bd3feb59854fdf08e7c767f2f385a86f9
aidan-mccarthy <amccarthy.business@gmail.com>
2019-10-02 13:51:38 -0400

    Update 0.4.0
    [+] Add user sessions
    [+] Add login/logout/register/profile functionality
    [+] Add environment variables
    [+] Update database table view

--- index.js
+++ index.js
@@ -17,35 +17,136 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Body Parser Setup
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Crypto Setup
+var crypto = require('crypto');
+const SALT = process.env.PASS_SALT;
+
+// Session Setup
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+app.use(session({
+  secret: process.env.SESS_SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(cookieParser());
+
 // Routing
 app.get('/', function(req, res) {
-  res.render('pages/index');
+  res.render('pages/index', { user: req.session.user });
 });
 
 app.get('/academics', function(req, res) {
-  res.render('pages/academics');
+  res.render('pages/academics', { user: req.session.user });
 });
 
 app.get('/military', function(req, res) {
-  res.render('pages/military');
+  res.render('pages/military', { user: req.session.user });
 });
 
 app.get('/physical', function(req, res) {
-  res.render('pages/physical');
+  res.render('pages/physical', { user: req.session.user });
+});
+
+app.get('/profile', function(req, res) {
+  res.render('pages/profile', { user: req.session.user });
+});
+
+app.get('/login', function(req, res) {
+  if (req.session.user) {
+    res.redirect('/profile');
+  } else {
+    res.render('pages/login', { user: null });
+  }
+});
+
+app.post('/login', async(req, res) => {
+  const client = await pool.connect();
+  data = { user: req.session.user };
+  console.log("Got POST to /login");
+  try {
+    var em = req.body.em;
+    var pw = crypto.pbkdf2Sync(req.body.pw, SALT, 1000, 64, 'sha256').toString('hex');
+
+    var query = 'SELECT xnumber FROM cadet WHERE email = \''+em+'\' AND password = \''+pw+'\';'
+    const result = await client.query(query);
+
+    if (result.rows) {
+      req.session.user = result.rows[0].xnumber;
+      res.redirect('/profile');
+    } else {
+      let msg = "Login Incorrect";
+      res.render('pages/login', { user: null, msg: msg });
+    }
+  } catch (e) {
+    console.log(e.stack);
+    let error = "System error: " + e.stack;
+    res.render('pages/error', { user: req.session.user, error: error });
+  } finally {
+    client.release();
+  }
+});
+
+app.get('/logout', function(req, res) {
+  req.session.user = null;
+  res.redirect('/');
+});
+
+app.get('/register', function(req, res) {
+  if (req.session.user) {
+    res.redirect('/profile');
+  } else {
+    res.render('pages/register', { user: null });
+  }
+});
+
+app.post('/register', async(req, res) => {
+  const client = await pool.connect();
+  try {
+    var em = req.body.em;
+    var xn = req.body.xn;
+    var ln = req.body.ln;
+    var fn = req.body.fn;
+    var mi = req.body.mi;
+    var pw = crypto.pbkdf2Sync(req.body.pw, SALT, 1000, 64, 'sha256').toString('hex');
+
+    await client.query('BEGIN');
+    var query = 'INSERT INTO cadet (xnumber,firstname,lastname,middleinitial,email,password) VALUES (\''+xn+'\',\''+fn+'\',\''+ln+'\',\''+mi+'\',\''+em+'\',\''+pw+'\');';
+    console.log('Query: ' + query);
+    const result = await client.query(query);
+
+    await client.query('COMMIT');
+    req.session.user = xn;
+    data.user = req.session.user;
+    res.render('pages/profile', { user: req.session.user });
+  } catch (e) {
+    await client.query('ROLLBACK');
+    console.log(e.stack);
+    let error = "System error: " + e.stack;
+    res.render('pages/error', { user: req.session.user, error: error });
+  } finally {
+    client.release();
+  }
 });
 
 app.get('/db', async(req, res) => {
+  const client = await pool.connect();
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT * FROM cadets');
-    const results = { 'results': (result) ? result.rows : null };
-    res.render('pages/db', results);
+    const result = await client.query('SELECT academicyear,firstname,lastname,middleinitial,platoon,squad,room,major,mentorship FROM cadet');
+    var results = { 'rows': (result) ? result.rows : null };
+    res.render('pages/db', { user: req.session.user, data: results });
+  } catch(e) {
+    let error = "System error: " + e.stack;
+    console.log(error);
+    res.render('pages/error', { user: req.session.user, error: error });
+  } finally {
     client.release();
-  }
-
-  catch (err) {
-    console.error(err);
-    res.send(err);
   }
 });
 

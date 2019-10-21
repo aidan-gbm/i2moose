ee5b33213294f52e9292116299c2643b1f2a3be8
aidan-mccarthy <amccarthy.business@gmail.com>
2019-10-21 08:09:33 -0400

    Update 0.5.0
    [+] Add more in-depth error handling for db backend
    [#] Consolidate all pgsql interaction into postgresql.js
    [-] Remove mentorship field for users

--- index.js
+++ index.js
@@ -1,172 +1,135 @@
 // Global Setup
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
-const pgsqlModule = require('./postgresql.js');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-// Database Pool Setup
-const { Pool } = require('pg');
-const conn = process.env.DATABASE_URL;
-const pool = new Pool({
-  connectionString: conn,
-  ssl: true
-});
+// Database Setup
+const pgsqlModule = require('./postgresql.js')
+pgsqlModule.setup(false)
 
 // Express Setup
-var app = express();
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+var app = express()
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 // Body Parser Setup
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Crypto Setup
-var crypto = require('crypto');
-const SALT = process.env.PASS_SALT;
+var crypto = require('crypto')
+const SALT = process.env.PASS_SALT
 
 // Session Setup
-var session = require('express-session');
-var cookieParser = require('cookie-parser');
+var session = require('express-session')
+var cookieParser = require('cookie-parser')
 app.use(session({
   secret: process.env.SESS_SECRET,
   resave: false,
   saveUninitialized: true
-}));
-app.use(cookieParser());
+}))
+app.use(cookieParser())
 
 // Routing
 app.get('/', function(req, res) {
-  res.render('pages/index', { user: req.session.user });
-});
+  res.render('pages/index', { user: req.session.user })
+})
 
 app.get('/academics', function(req, res) {
-  res.render('pages/academics', { user: req.session.user });
-});
+  res.render('pages/academics', { user: req.session.user })
+})
 
 app.get('/military', function(req, res) {
-  res.render('pages/military', { user: req.session.user });
-});
+  res.render('pages/military', { user: req.session.user })
+})
 
 app.get('/physical', function(req, res) {
-  res.render('pages/physical', { user: req.session.user });
-});
+  res.render('pages/physical', { user: req.session.user })
+})
 
 app.get('/profile', async(req, res) => {
   if (req.session.user) {
-    const client = await pool.connect();
-    try {
-      let xn = req.session.user;
-      let query = 'SELECT firstname,lastname,middleinitial,academicyear,phonenumber,platoon,squad,room,major,mentorship,email FROM cadet WHERE xnumber = \'' + xn + '\';';
-      let result = await client.query(query);
+    let xn = req.session.user
+    let result = await pgsqlModule.getProfile(xn)
+    console.log(result.rows)
 
-      if (result.rows[0]) {
-        res.render('pages/profile', { user: xn, data: result.rows[0] });
-      } else {
-        let error = "Somehow you're logged in but not in the database..."
-        res.render('pages/error', { user: req.session.user, error: error });
-      }
-    } catch (e) {
-      console.log(e.toString());
-      let error = "System error: " + e.toString();
-      res.render('pages/error', { user: req.session.user, error: error });
-    } finally {
-      client.release();
+    if (result.rows[0]) {
+      res.render('pages/profile', { user: xn, data: result.rows[0] })
+    } else {
+      let error = "Somehow you're logged in but not in the database..."
+      res.render('pages/error', { user: req.session.user, error: error })
     }
   } else {
-    res.redirect('/login');
+    res.redirect('/login')
   }
-});
+})
 
 app.get('/login', function(req, res) {
   if (req.session.user) {
-    res.redirect('/profile');
+    res.redirect('/profile')
   } else {
-    res.render('pages/login', { user: null });
+    res.render('pages/login', { user: null })
   }
-});
+})
 
 app.post('/login', async(req, res) => {
-  const client = await pool.connect();
-  try {
-    let em = req.body.em;
-    let pw = crypto.pbkdf2Sync(req.body.pw, SALT, 1000, 64, 'sha256').toString('hex');
+  let em = req.body.em
+  let pw = crypto.pbkdf2Sync(req.body.pw, SALT, 1000, 64, 'sha256').toString('hex')
+  let result = await pgsqlModule.getXnumber(em, pw)
 
-    let query = 'SELECT xnumber FROM cadet WHERE email = \''+em+'\' AND password = \''+pw+'\' LIMIT 1;'
-    let result = await client.query(query);
-    if (result.rows[0]) {
-      req.session.user = result.rows[0].xnumber;
-      res.redirect('/profile');
-    } else {
-      let msg = "Login Incorrect";
-      res.render('pages/login', { user: null, msg: msg });
-    }
-  } catch (e) {
-    console.log(e.toString());
-    let error = "System error: " + e.toString();
-    res.render('pages/error', { user: req.session.user, error: error });
-  } finally {
-    client.release();
+  if (result.rows[0]) {
+    req.session.user = result.rows[0].xnumber
+    res.redirect('/profile')
+  } else {
+    let msg = "Login Incorrect"
+    res.render('pages/login', { user: null, msg: msg })
   }
-});
+})
 
 app.get('/logout', function(req, res) {
-  req.session.user = null;
-  res.redirect('/');
-});
+  req.session.user = null
+  res.redirect('/')
+})
 
 app.get('/register', function(req, res) {
   if (req.session.user) {
-    res.redirect('/profile');
+    res.redirect('/profile')
   } else {
-    res.render('pages/register', { user: null });
+    res.render('pages/register', { user: null })
   }
-});
+})
 
 app.post('/register', async(req, res) => {
-  const client = await pool.connect();
-  try {
-    var em = req.body.em;
-    var xn = req.body.xn;
-    var ln = req.body.ln;
-    var fn = req.body.fn;
-    var mi = req.body.mi;
-    var pw = crypto.pbkdf2Sync(req.body.pw, SALT, 1000, 64, 'sha256').toString('hex');
+  var em = req.body.em
+  var xn = req.body.xn
+  var ln = req.body.ln
+  var fn = req.body.fn
+  var mi = req.body.mi
+  var pw = crypto.pbkdf2Sync(req.body.pw, SALT, 1000, 64, 'sha256').toString('hex')
 
-    await client.query('BEGIN');
-    if (mi) pgsqlModule.register(client, xn, em, pw, fn, ln, mi);
-    else pgsqlModule.register(client, xn, em, pw, fn, ln);
+  let response = await pgsqlModule.register(xn, em, pw, fn, ln, mi)
 
-    await client.query('COMMIT');
+  if (response) {
     req.session.user = xn;
-    res.render('pages/profile', { user: req.session.user });
-  } catch (e) {
-    await client.query('ROLLBACK');
-    console.log(e.toString());
-    let error = "System error: " + e.toString();
-    res.render('pages/error', { user: req.session.user, error: error });
-  } finally {
-    client.release();
+    res.redirect('/profile')
+  } else {
+    let err = 'Server error. Contact administrator.'
+    res.render('pages/error', { user: null, error: err })
   }
-});
+})
 
 app.get('/roster', async(req, res) => {
-  const client = await pool.connect();
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT academicyear,firstname,lastname,middleinitial,platoon,squad,room,major,mentorship FROM cadet');
-    var results = { 'rows': (result) ? result.rows : null };
-    res.render('pages/roster', { user: req.session.user, data: results });
-  } catch(e) {
-    let error = "System error: " + e.toString();
-    console.log(error);
-    res.render('pages/error', { user: req.session.user, error: error });
-  } finally {
-    client.release();
+  let result = await pgsqlModule.getRoster(false)
+  if (result) {
+    let results = { 'rows': (result) ? result.rows : null }
+    res.render('pages/roster', { user: req.session.user, data: results })
+  } else {
+    let err = 'Server error. Contact administrator.'
+    res.render('pages/error', { user: null, error: err })
   }
-});
+})
 
 app.post('/update-user', async(req, res) => {
   if (!req.session.user) {
@@ -174,44 +137,25 @@ app.post('/update-user', async(req, res) => {
     res.render('pages/error', { user: null, error: error });
   }
   else {
-    const client = await pool.connect();
-    try {
-      let fn = (req.body.fn) != '' ? '\''+req.body.fn+'\'' : null;
-      let ln = (req.body.ln) != '' ? '\''+req.body.ln+'\'' : null;
-      let mi = (req.body.mi) != '' ? '\''+req.body.mi+'\'' : null;
-      let ay = (req.body.ay) != '' ? req.body.ay : null;
-      let pl = (req.body.pl) != '' ? req.body.pl : null;
-      let sq = (req.body.sq) != '' ? req.body.sq : null;
-      let rm = (req.body.rm) != '' ? req.body.rm : null;
-      let mj = (req.body.mj) != '' ? '\''+req.body.mj+'\'' : null;
-      let mn = (req.body.mn) != '' ? '\''+req.body.mn+'\'' : null;
-      let xn = req.session.user;
+    let data = {}
+    data['firstname'] = (!!req.body.fn) ? `'${req.body.fn}'` : null
+    data['lastname'] = (!!req.body.ln) ? `'${req.body.ln}'` : null
+    data['middleinitial'] = (!!req.body.mi) ? `'${req.body.mi}'` : null
+    data['academicyear'] = (!!req.body.ay) ? req.body.ay : null
+    data['platoon'] = (!!req.body.pl) ? req.body.pl : null
+    data['squad'] = (!!req.body.sq) ? req.body.sq : null
+    data['room'] = (!!req.body.rm) ? req.body.rm : null
+    data['major'] = (!!req.body.mj) ? `'${req.body.mj}'` : null
+    let xn = req.session.user
 
-      await client.query('BEGIN');
-      let query = 'UPDATE cadet SET firstName='+fn+
-        ',lastName='+ln+
-        ',middleInitial='+mi+
-        ',academicYear='+ay+
-        ',platoon='+pl+
-        ',squad='+sq+
-        ',room='+rm+
-        ',major='+mj+
-        ',mentorship='+mn+
-        ' WHERE xnumber = \''+ xn + '\';';
-      const result = await client.query(query);
-
-      await client.query('COMMIT');
-      res.redirect('/profile');
-    } catch (e) {
-      await client.query('ROLLBACK');
-      console.log(e.toString());
-      let error = e.toString();
-      res.render('pages/error', { user: req.session.user, error: error });
-    } finally {
-      client.release();
+    if (await pgsqlModule.updateUser(data, xn))
+      res.redirect('/profile')
+    else {
+      let err = 'Server error. Contact administrator.'
+      res.render('pages/error', { user: null, error: err })
     }
   }
-});
+})
 
 // Server Listen
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
--- postgresql.js
+++ postgresql.js
@@ -1,6 +1,44 @@
-export function setup(client) {
-    createCadet = `
-        CREATE TABLE IF NOT EXISTS cadet (
+// Connect to db & create pool
+const conn = process.env.DATABASE_URL;
+const { Pool } = require('pg')
+const pool = new Pool({
+  connectionString: conn,
+  ssl: true
+})
+
+// Send error on idle clients
+pool.on('error', (err, client) => {
+    console.error('Unexpected error on idle client', err)
+    process.exit(-1)
+})
+
+// Attempt query on database
+;(query = async(queryString) => {
+    if (!queryString) return
+    let client = await pool.connect()
+    try {
+        await client.query('BEGIN')
+        let res = await client.query(queryString)
+        await client.query('COMMIT')
+        return res
+    } catch (e) {
+        await client.query('ROLLBACK')
+        console.log(e.toString())
+        console.log('Query:')
+        console.log(queryString)
+        return false
+    } finally {
+        client.release()
+    }
+})().catch(e => console.log(e.toString()))
+
+/********** SETUP TABLES ***********/
+exports.setup = async(clean) => {
+    cleanTables = `
+        DROP TABLE IF EXISTS cadettasked;
+        DROP TABLE IF EXISTS training;
+        DROP TABLE IF EXISTS cadet;`
+    createCadet = `CREATE TABLE IF NOT EXISTS cadet (
             xnumber VARCHAR(6) PRIMARY KEY,
             email TEXT NOT NULL,
             password TEXT NOT NULL,
@@ -14,43 +52,88 @@ export function setup(client) {
             room INT,
             major VARCHAR(25)
         );`
-    createTraining = `
-        CREATE TABLE IF NOT EXISTS training (
+    createTraining = `CREATE TABLE IF NOT EXISTS training (
             id SERIAL PRIMARY KEY,
             name VARCHAR(50) NOT NULL,
             suspense DATE,
             description TEXT
         );`
-    createCadetTasked = `
-        CREATE TABLE IF NOT EXISTS cadettasked (
+    createCadetTasked = `CREATE TABLE IF NOT EXISTS cadettasked (
             eventid INT REFERENCES training(id) ON UPDATE CASCADE,
             cadetid VARCHAR(6) REFERENCES cadet(xnumber) ON UPDATE CASCADE
         );`
-    
-    let res1 = await client.query(createCadet);
-    let res2 = await client.query(createTraining);
-    let res3 = await client.query(createCadetTasked);
-    return [res1,res2,res3]
+    if (clean) {
+        return (
+            await query(cleanTables) &&
+            await query(createTraining) &&
+            await query(createCadet) &&
+            await query(createCadetTasked)
+        )
+    } else {
+        return (
+            await query(createTraining) &&
+            await query(createCadet) &&
+            await query(createCadetTasked)
+        )
+    }
 }
 
-// Register with middle initial
-export function register(client, xn, email, pass, fn, ln, mi) {
-    register = `
-        CALL register(
-            ${xn}, ${email}, ${pass}, ${fn}, ${ln}, ${mi}
-        );`
+/********** REGISTER ***********/
+exports.register = async(xn, email, pass, fn, ln, mi) => {
+    register = `INSERT INTO cadet (
+            xnumber,
+            email,
+            password,
+            firstname,
+            lastname,
+            middleinitial
+        ) VALUES (
+            '${xn}', '${email}', '${pass}', '${fn}', '${ln}', '${mi}'
+        ) ON CONFLICT (xnumber) DO NOTHING;`
     
-    let res = await client.query(query);
-    return res;
+    return await query(register)
 }
 
-// Register without middle initial
-export function register(client, xn, email, pass, fn, ln) {
-    register = `
-    CALL register(
-        ${xn}, ${email}, ${pass}, ${fn}, ${ln}
-    );`
+/********** GET XNUMBER ***********/
+exports.getXnumber = async(email, password) => {
+    getXnumber = `SELECT xnumber
+        FROM cadet
+        WHERE email = '${email}'
+        AND password = '${password}'
+        LIMIT 1;`
+    return await query(getXnumber)
+}
 
-    let res = await client.query(query);
-    return res;
+/********** GET PROFILE ***********/
+exports.getProfile = async(xnumber) => {
+    getProfile = `SELECT *
+        FROM cadet
+        WHERE xnumber = '${xnumber}';`
+    return await query(getProfile)
+}
+
+/********** GET ROSTER ***********/
+exports.getRoster = async(admin) => {
+    getRoster = `SELECT
+            academicyear,
+            firstname,
+            lastname,
+            middleinitial,
+            platoon,
+            squad,
+            room,
+            major
+        FROM cadet;`
+    return await query(getRoster)
+}
+
+/********** UPDATE USER ***********/
+exports.updateUser = async(data, xnumber) => {
+    updateUser = `UPDATE cadet SET`
+    for (let field in data) {
+        let value = data[field]
+        updateUser = updateUser + ` ${field}=${value},`
+    }
+    updateUser = updateUser.slice(0,updateUser.length-1) + ` WHERE xnumber = '${xnumber}';`
+    return await query(updateUser)
 }

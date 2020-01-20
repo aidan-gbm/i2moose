93173bcd4378b158aafddd0fc5fe23d409846837
aidan-mccarthy <amccarthy.business@gmail.com>
2020-01-20 12:11:28 -0500

    Update 1.1.5
    [+] Fix bug with posts on home page
    [#] Move home routing to main router

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app = express()
 const path = require('path')
 
 // Database
-const modulePostgres = require('./postgresql.js')
-modulePostgres.setup()
+//const modulePostgres = require('./postgresql.js')
+//modulePostgres.setup()
 
 // Font Awesome
 app.set('fa-js', path.join(__dirname, 'node_modules', '@fortawesome', 'fontawesome-free', 'js'))
@@ -33,9 +33,6 @@ app.use(session({
 const cookieParser = require('cookie-parser')
 app.use(cookieParser())
 
-// Renderer
-const renderer = require('./renderer')
-
 // Set Variables
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
@@ -53,76 +50,9 @@ app.use('/staff', staffRouter)
 const profileRouter = require('./routers/profile')
 app.use('/profile', profileRouter)
 
-// Home
-app.get('/', async function(req, res) {
-  let home = await modulePostgres.getPosts('home')
-  let history = await modulePostgres.getPosts('history')
-  if (home.rows[0] || history.rows[0]) {
-    let data = {
-      'posts': {
-        'home': home.rows,
-        'history': history.rows
-      }
-    }
-    data['posts']['home'].forEach(p => {
-      // console.log('Title: ' + Buffer.from(p.title, 'base64').toString())
-      // console.log('Text: ' + Buffer.from(p.text, 'base64').replace(/(\r\n)/g))
-      // console.log('Date: ' + p.date)
-      // console.log('Author: ' + p.author)
-    })
-    renderer.renderPage(res, 'pages/index', req.session.user, data)
-  } else {
-    renderer.renderPage(res, 'pages/index', req.session.user)
-  }
-})
-
-// Roster
-app.get('/roster', async function(req, res) {
-  let result = await modulePostgres.getRoster()
-  if (result) {
-    let results = { 'rows': (result.rows[0]) ? result.rows : null }
-    renderer.renderPage(res, 'pages/roster', req.session.user, results)
-  } else {
-    renderer.errors.push({'msg':'Server error. Contact administrator.'})
-    renderer.renderPage(res, 'pages/roster', null, {})
-  }
-})
-
-// Academics
-app.get('/academics', async function(req, res) {
-  let result = await modulePostgres.getPosts('academics')
-  if (result.rows[0]) {
-    let data = {'posts': result.rows}
-    console.log(result.rows)
-    renderer.renderPage(res, 'pages/academics', req.session.user, data)
-  } else {
-    renderer.renderPage(res, 'pages/academics', req.session.user)
-  }
-})
-
-// Military
-app.get('/military', async function(req, res) {
-  let result = await modulePostgres.getPosts('academics')
-  if (result.rows[0]) {
-    let data = {'posts': result.rows}
-    console.log(result.rows)
-    renderer.renderPage(res, 'pages/military', req.session.user, data)
-  } else {
-    renderer.renderPage(res, 'pages/military', req.session.user)
-  }
-})
-
-// Physical
-app.get('/physical', async function(req, res) {
-  let result = await modulePostgres.getPosts('academics')
-  if (result.rows[0]) {
-    let data = {'posts': result.rows}
-    console.log(result.rows)
-    renderer.renderPage(res, 'pages/physical', req.session.user, data)
-  } else {
-    renderer.renderPage(res, 'pages/physical', req.session.user)
-  }
-})
+// Main (Sub)
+const mainRouter = require('./routers/main')
+app.use('/', mainRouter)
 
 // Server Listen
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
---
+++ routers/main.js
@@ -0,0 +1,83 @@
+/***************/
+/*   MODULES   */
+/***************/
+
+// Router
+const router = require('express').Router()
+
+// Database
+const modulePostgres = require('../postgresql')
+
+// Renderer
+const renderer = require('../renderer')
+
+/***************/
+/*   ROUTING   */
+/***************/
+
+// Home
+router.get('/', async function(req, res) {
+  let home = await modulePostgres.getPosts('home')
+  let history = await modulePostgres.getPosts('history')
+  if (home.rows[0] || history.rows[0]) {
+    let data = {
+      'posts': {
+        'home': home.rows,
+        'history': history.rows
+      }
+    }
+    renderer.renderPage(res, 'pages/index', req.session.user, data)
+  } else {
+    renderer.renderPage(res, 'pages/index', req.session.user)
+  }
+})
+
+// Roster
+router.get('/roster', async function(req, res) {
+  let result = await modulePostgres.getRoster()
+  if (result) {
+    let results = { 'rows': (result.rows[0]) ? result.rows : null }
+    renderer.renderPage(res, 'pages/roster', req.session.user, results)
+  } else {
+    renderer.errors.push({'msg':'Server error. Contact administrator.'})
+    renderer.renderPage(res, 'pages/roster', null, {})
+  }
+})
+
+// Academics
+router.get('/academics', async function(req, res) {
+  let result = await modulePostgres.getPosts('academics')
+  if (result.rows[0]) {
+    let data = {'posts': result.rows}
+    console.log(result.rows)
+    renderer.renderPage(res, 'pages/academics', req.session.user, data)
+  } else {
+    renderer.renderPage(res, 'pages/academics', req.session.user)
+  }
+})
+
+// Military
+router.get('/military', async function(req, res) {
+  let result = await modulePostgres.getPosts('academics')
+  if (result.rows[0]) {
+    let data = {'posts': result.rows}
+    console.log(result.rows)
+    renderer.renderPage(res, 'pages/military', req.session.user, data)
+  } else {
+    renderer.renderPage(res, 'pages/military', req.session.user)
+  }
+})
+
+// Physical
+router.get('/physical', async function(req, res) {
+  let result = await modulePostgres.getPosts('academics')
+  if (result.rows[0]) {
+    let data = {'posts': result.rows}
+    console.log(result.rows)
+    renderer.renderPage(res, 'pages/physical', req.session.user, data)
+  } else {
+    renderer.renderPage(res, 'pages/physical', req.session.user)
+  }
+})
+
+module.exports = router
